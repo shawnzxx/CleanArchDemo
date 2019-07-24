de9c013598ba32b910a7647da7fa6beb17978fe8
shawnzxx <shawnzhang.dev@gmail.com>
2019-07-24 23:28:41 +0800

    Implemented service and repository to return courses entity set

---
+++ CleanArch/CleanArch.Application/Services/CourseService.cs
@@ -0,0 +1,28 @@
+﻿using CleanArch.Application.Interfaces;
+using CleanArch.Application.ViewModels;
+using CleanArch.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArch.Application.Services
+{
+    public class CourseService : ICourseService
+    {
+        private readonly ICourseRepository _courseRepository;
+
+        public CourseService(ICourseRepository courseRepository)
+        {
+            _courseRepository = courseRepository;
+        }
+
+        public CourseViewModel GetCourses()
+        {
+            var courses = _courseRepository.GetCourses();
+            return new CourseViewModel()
+            {
+                Courses = courses
+            };
+        }
+    }
+}
---
+++ CleanArch/CleanArch.Infra.Data/Repository/CourseRepository.cs
@@ -0,0 +1,24 @@
+﻿using CleanArch.Domain.Interfaces;
+using CleanArch.Domain.Models;
+using CleanArch.Infra.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArch.Infra.Data.Repository
+{
+    public class CourseRepository : ICourseRepository
+    {
+        private readonly UniversityDBContext _universityDBContext;
+
+        public CourseRepository(UniversityDBContext universityDBContext)
+        {
+            _universityDBContext = universityDBContext;
+        }
+
+        public IEnumerable<Course> GetCourses()
+        {
+            return _universityDBContext.Courses;
+        }
+    }
+}
