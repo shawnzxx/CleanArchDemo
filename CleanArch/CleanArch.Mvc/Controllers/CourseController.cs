ba998928ca7474951adc050c10a6970442be7a78
shawnzxx <shawnzhang.dev@gmail.com>
2019-07-26 15:21:38 +0800

    presentation course controller view style and authentication

--- CleanArch/CleanArch.Mvc/Controllers/CourseController.cs
+++ CleanArch/CleanArch.Mvc/Controllers/CourseController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using CleanArch.Application.Interfaces;
 using CleanArch.Application.ViewModels;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -20,6 +21,7 @@ namespace CleanArch.Mvc.Controllers
         }
 
         // GET: /<controller>/
+        [Authorize]
         public IActionResult Index()
         {
             CourseViewModel model = _courseService.GetCourses();
