ac75baffd60eb0100114da1c33cc2d03479319fa
shawnzxx <shawnzhang.dev@gmail.com>
2019-07-26 14:45:36 +0800

    Add new course controller and it's css and view

--- CleanArch/CleanArch.Infra.Data/Context/UniversityDBContext.cs
+++ CleanArch/CleanArch.Infra.Data/Context/UniversityDBContext.cs
@@ -8,7 +8,7 @@ namespace CleanArch.Infra.Data.Context
 {
     public class UniversityDBContext : DbContext
     {
-        public UniversityDBContext(DbContextOptions options) : base(options)
+        public UniversityDBContext(DbContextOptions<UniversityDBContext> options) : base(options)
         {
         }
 
