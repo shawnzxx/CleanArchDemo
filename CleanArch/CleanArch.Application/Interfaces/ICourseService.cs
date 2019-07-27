41b2e453f17f9cf020fd5322450739b1f3186f7a
shawnzxx <shawnzhang.dev@gmail.com>
2019-07-27 18:07:41 +0800

    add new api project and configure

--- CleanArch/CleanArch.Application/Interfaces/ICourseService.cs
+++ CleanArch/CleanArch.Application/Interfaces/ICourseService.cs
@@ -8,5 +8,7 @@ namespace CleanArch.Application.Interfaces
     public interface ICourseService
     {
         CourseViewModel GetCourses();
+
+        void Create(CourseViewModel courseViewModel);
     }
 }
